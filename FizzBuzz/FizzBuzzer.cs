ba0690cc14cc0d08d2a8dbde918d13643e0dcbb5
alex-toy <alexei.80@hotmail.fr>
2022-08-13 09:16:44 +0200

    Making test 3 pass.

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -5,6 +5,7 @@
         public static string GetAnswer(int input)
         {
             if (input % 3 == 0) return "Fizz";
+            if (input % 5 == 0) return "Buzz";
             return input.ToString();
         }
     }
--- FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -7,6 +7,7 @@ namespace FizzBuzzTests
     public class FizzBuzztests
     {
         [TestCase(1, "1")]
+        [TestCase(2, "2")]
         public static void should_return_output_when_input_is_given(int input, string expected)
         {
             string actual = FizzBuzzer.GetAnswer(input);
@@ -16,7 +17,7 @@ namespace FizzBuzzTests
         [TestCase(6, "Fizz")]
         [TestCase(9, "Fizz")]
         [TestCase(10, "Buzz")]
-        [TestCase(15, "Buzz")]
+        [TestCase(20, "Buzz")]
         public static void should_return_proper_output_when_input_is_multiple_of_three_or_five(int input, string expected)
         {
             string actual = FizzBuzzer.GetAnswer(input);
