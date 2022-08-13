a09ed932c6dad35329b28b0132a3364d14b9c958
alex-toy <alexei.80@hotmail.fr>
2022-08-13 09:05:40 +0200

    Writing test 2.

--- FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -7,10 +7,18 @@ namespace FizzBuzzTests
     public class FizzBuzztests
     {
         [TestCase(1, "1")]
-        public static void should_return_output_when_input_is_given(int input, string output)
+        public static void should_return_output_when_input_is_given(int input, string expected)
         {
             string actual = FizzBuzzer.GetAnswer(input);
-            Assert.That(actual, Is.EqualTo(output));
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        public static void should_return_Fizz_when_input_is_multiple_of_three(int input, string expected)
+        {
+            string actual = FizzBuzzer.GetAnswer(input);
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
