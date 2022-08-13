47800c05c4f0759e5c0731373ca16bf126333e1b
alex-toy <alexei.80@hotmail.fr>
2022-08-13 09:35:05 +0200

    Extracting method.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,9 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to FizzBuzz!");
-            for(int i = 0; i <= 100; i++) {
+            ShowResult();
+        }
+        
+        private static void ShowResult()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
                 string output = FizzBuzzer.GetAnswer(i);
-                Console.WriteLine($"{i} -> {output}"); 
+                Console.WriteLine($"{i} -> {output}");
             }
         }
     }
