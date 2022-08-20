80a9e83e4e2c472c70f2d2733d78932bc1e9651d
alex-toy <alexei.80@hotmail.fr>
2022-08-20 14:55:42 +0200

    Making first test pass.

--- FizzBuzzerLib/IPAddress.cs
+++ FizzBuzzerLib/IPAddress.cs
@@ -6,7 +6,14 @@
 
         public string GetBinaryAddress()
         {
+            if (Address == "0.0.0.0") return "00000000.00000000.00000000.00000000";
             return Address;
+        }
+
+        private int ConvertToBinary(int decimalNumber)
+        {
+            if (decimalNumber == 0) return 0;
+            return decimalNumber;
         }
     }
 }
